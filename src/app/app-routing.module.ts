a5c9e0333453a0b71e0179c93cb49c35cae8edbe
Kailash <kailashsridhar8@gmail.com>
2020-04-08 23:59:24 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SinginComponent } from './singin/singin.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: HomeComponent
+  },
+  {
+    path:'login', component: LoginComponent
+  },
+  {
+    path:'signin', component: SinginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent,LoginComponent,SinginComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SinginComponent } from './singin/singin.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    routingComponents,
+    HeaderComponent,
+    SinginComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
